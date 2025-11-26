fe63a2d69136a04168c1e56a75b74e6b8b622f6d
sanjay <sanjaygolappanvar1@gmail.com>
2025-11-26 13:01:14 +0530

    Add PWA manifest, service worker, and icons for APK build

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'chat-pwa-v1';
+const OFFLINE_URL = '/';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((r) => r || caches.match(OFFLINE_URL));
+      })
+  );
+});
